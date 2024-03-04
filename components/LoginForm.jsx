58d4142bde97b12db77629476ad65012a840bd78
Beterraba <ls6182315@gmail.com>
2024-03-04 16:35:10 -0300

    add submit handler

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -1,27 +1,57 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export const LoginForm = () => {
-  const password = "erttffd";
-  const email = true;
-  const isValidPas = password.length > 5;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log({ email, password });
+
+    try {
+      const response = await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+      });
+
+      if (response.error) {
+        setError("Invalid credentials");
+        return;
+      }
+
+      router.replace("dashboad");
+    } catch (error) {}
+  };
   return (
     <div className="grid place-items-center h-screen">
       <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
         <h1 className="text-xl font-bold my-4">Login</h1>
-        <form action="" className="flex flex-col gap-3">
+        <form action="" onSubmit={handleSubmit} className="flex flex-col gap-3">
           <input
             type="email"
             name="email-input"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             id=""
             required
             placeholder="Email"
           />
           <input
-            type="password"
             name="pass-input"
-            id=""
-            required
+            onChange={(e) => setPassword(e.target.value)}
             placeholder="Password"
+            required
+            type="password"
+            value={password}
           />
 
           <button
@@ -31,9 +61,11 @@ export const LoginForm = () => {
           >
             Login
           </button>
-          <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
-            Error message
-          </div>
+          {error && (
+            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+              {erro}
+            </div>
+          )}
           <Link className="text-sm mt-3 text-right" href={"/register"}>
             Don&apos;t have an account?{" "}
             <span className="underline">Register</span>
