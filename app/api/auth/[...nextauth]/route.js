4737cbbbfa31c7272ec1b50518de8b53da1f8986
Beterraba <ls6182315@gmail.com>
2024-03-04 23:38:53 -0300

    auth next validation

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,14 +1,35 @@
 import NextAuth from "next-auth/next";
-import CredentialsProvider from "next-auth/providers";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-const authOptions = {
+export const authOptions = {
   providers: [
     CredentialsProvider({
       name: "credentials",
       credentials: {},
       async authorize(credentials) {
-        const user = { id: "1" };
-        return user;
+        const { email, password } = credentials;
+
+        try {
+          const response = await fetch(
+            `${process.env.NEXT_PUBLIC_API_URL}/user/login`,
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ email, password }),
+            }
+          );
+
+          const { token } = await response.json();
+          if (!token) return null;
+          const user = { email };
+
+          user.token = token;
+          return user;
+        } catch (error) {
+          console.log("Error: ", error);
+        }
       },
     }),
   ],
@@ -16,6 +37,15 @@ const authOptions = {
     strategy: "jwt",
   },
   secret: process.env.NEXT_PUBLIC_JTW_SECRET,
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+    async session(session, token, user) {
+      session.user = token;
+      return session;
+    },
+  },
   pages: {
     signIn: "/",
   },
