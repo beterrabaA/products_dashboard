7dd557083ee6fc01733037779babd54346b26f40
Beterraba <ls6182315@gmail.com>
2024-03-06 16:17:04 -0300

    empty list component

---
+++ components/EmptyList.jsx
@@ -0,0 +1,7 @@
+export const EmptyList = () => {
+  return (
+    <div className="flex w-full items-center justify-center">
+      <h1 className="font-bold text-lg py-5">Empty list</h1>
+    </div>
+  );
+};
