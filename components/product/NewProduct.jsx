6552333b291402bd0c1ab3964752a2b5e7934de3
Beterraba <ls6182315@gmail.com>
2024-03-06 16:16:43 -0300

    new product button

---
+++ components/product/NewProduct.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useState } from "react";
+import { CardCreate } from "./CardCreate";
+
+export const NewProduct = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="flex flex-col md:flex-row w-max-[150px] justify-between">
+      <p />
+      <button
+        className="m-2 p-3 bg-gray-400 rounded-md text-white font-bold"
+        onClick={() => setIsOpen(true)}
+      >
+        New product
+      </button>
+      <CardCreate isOpen={isOpen} closeModal={() => setIsOpen(false)} />
+    </div>
+  );
+};
